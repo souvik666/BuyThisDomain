49e3494f7fa1f455c181be5b6c02bbedae3d6e88
souvik666 <peacedynasty0@gmail.com>
2025-03-17 09:50:15 +0530

    [2025-03-17 09:50:15] 🛠 UPDATED: src/services/animal.service.ts

--- src/services/animal.service.ts
+++ src/services/animal.service.ts
@@ -1,19 +1,11 @@
 class AnimalService {
-    private static API_SOURCES = [
-        { url: "https://random.dog/woof.json", key: "url" },
-        { url: "https://aws.random.cat/meow", key: "file" },
-        { url: "https://randomfox.ca/floof/", key: "image" },
-        { url: "https://random.dog/woof.json", key: "url" },
-  
-    ];
+    private static API_SOURCES = "https://random.dog/woof.json"
 
     static async getRandomAnimalImage(): Promise<string> {
-        const randomSource = this.API_SOURCES[Math.floor(Math.random() * this.API_SOURCES.length)];
-
         try {
-            const response = await fetch(randomSource.url);
+            const response = await fetch(this.API_SOURCES);
             const data = await response.json();
-            return data[randomSource.key] || "";
+            return data?.url || "";
         } catch (error) {
             console.error("Error fetching animal image:", error);
             return "";
